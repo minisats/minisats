861e30973864da05dacae8a0c773017d84eb6055
minisat-maker <minisat-maker@safe-mail.net>
2013-06-02 15:44:41 -0800

    adding dependencies

---
+++ secret_values.php
@@ -0,0 +1,10 @@
+<?php
+$database_username = "dbusername";
+$database_pass = "dbpass";
+$database_name = "dbname";
+
+$rpcuser = "rpcuser";
+$rpcpassword = "rpcpass";
+$rpcip = "ip";
+$rpcport = "port";
+?>
